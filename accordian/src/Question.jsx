83985923d5e1d72769adff5f18f310bd2cafce17
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-11 23:01:27 +0530

    conditional rendering of the button text based on the state value of the variable.

--- accordian/src/Question.jsx
+++ accordian/src/Question.jsx
@@ -2,23 +2,31 @@ import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import PropTypes from "prop-types";
 
-function Question({title, info}){
-    const [showInfo, setShowInfo] = useState(false);
-    
-    return(
-        <article className="question">
-            <header>
-                <h4>{title}</h4>
-                <button type="button" className="btn" onClick={()=>{setShowInfo(!showInfo)}}>Btn</button>
-            </header>
-            {showInfo && <p>{info}</p>}
-        </article>
-    );
+function Question({ title, info }) {
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => {
+            setShowInfo(!showInfo);
+          }}
+        >
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 }
 
 Question.propTypes = {
-    title: PropTypes.string.isRequired,
-    info: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  info: PropTypes.string.isRequired,
 };
 
 export default Question;
