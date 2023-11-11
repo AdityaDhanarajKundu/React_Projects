19a1ad311f2e4c367f970dfac15bf3a2d35fbc8e
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-11 23:03:52 +0530

    complete

--- accordian/src/App.jsx
+++ accordian/src/App.jsx
@@ -1,12 +1,22 @@
-import Question from "./Question"
+import Question from "./Question";
+import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
 
   return (
-    <>
-      <Question />
-    </>
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question)=>{
+            return <Question key={question.id} {...question} />
+          })}
+        </section>
+      </div>
+    </main>
   )
 }
 
-export default App
+export default App;
