bddd6f7206611a1d85cfe366ac9f9830b9205149
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-12 13:16:40 +0530

    made the basic layout of the menu component

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,13 +1,24 @@
-
+import { useState } from "react"
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
 
   return (
-    <>
-      <h1>Menu</h1>
-    </>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   )
 }
 
-export default App
+export default App;
--- menu/src/Menu.jsx
+++ menu/src/Menu.jsx
@@ -1,7 +1,36 @@
-function Menu(){
+import PropTypes from 'prop-types';
+
+function Menu({items}){
     return(
-        <h2>Menu Component</h2>
+        <div className='section-center'>
+            {items.map((menuItem)=>{
+                const {id,title,img,desc,price} = menuItem;
+                return (
+                    <article className="menu-item" key={id}>
+                        <img src={img} alt={title} className='photo' />
+                        <div className="item-info">
+                            <header>
+                                <h4>{title}</h4>
+                                <h4 className={price}>{price}</h4>
+                            </header>
+                            <p className="item-text">{desc}</p>
+                        </div>
+                    </article>
+                );
+            })}
+        </div>
     );
 }
+
+Menu.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        img: PropTypes.string.isRequired,
+        desc: PropTypes.string.isRequired
+    }))
+};
 
 export default Menu;
