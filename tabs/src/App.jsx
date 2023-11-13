d3559c714e740a6e8dfe6e7913ed26d5ed6b3f3a
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-13 17:37:35 +0530

    added a fetch function to fetch the data from the url

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState,useEffect } from "react";
-import {FaAngleDoubleRight} from "react-icons/fa";
+import { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
 
+  //function to fetch the data from the url
+  async function fetchData() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setJobs(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // we want to fetch the data during the first time the application loads
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <h1>App</h1>
     </>
-  )
+  );
 }
 
 export default App;
