077d6ad633c8211ac297f9d8453a74b375970b36
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-13 13:13:31 +0530

    initial tabs project setup

---
+++ tabs/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState,useEffect } from "react";
+import {FaAngleDoubleRight} from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App;
