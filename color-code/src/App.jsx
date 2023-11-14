f07e002d6ef6459c5bcc26b0b9d831c52cff91c3
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-14 21:00:32 +0530

    initial color code generator project setup

---
+++ color-code/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import SingleColor from './SingleColor';
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>color generator</h2>
+    </>
+  )
+}
+
+export default App;
---
+++ color-code/src/SingleColor.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+function SingleColor(){
+    return <h4>single color component</h4>
+}
+
+export default SingleColor;
