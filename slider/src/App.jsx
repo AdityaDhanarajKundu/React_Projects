ff1c0fa84b8d8b00fd29337ab8cd65e17ce0d893
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-14 12:23:50 +0530

    made the basic layout of the component and added the slide animations, with one bug yet to be fixed

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -4,11 +4,44 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 
 function App() {
-
+  const [people,setPeople] = useState(data);
+  const [index,setIndex] = useState(2); // index of the people array
+  
   return (
-    <>
-      <h2>Slider setup</h2>
-    </>
+    <section className='section'>
+      <div className='title'>
+        <h2><span>/</span>reviews</h2>
+      </div>
+      <div className="section-center">
+        {people.map((person,personIndex)=>{
+          const {id,image,name,title,quote} = person;
+          // more stuff coming up
+          let position = 'nextSlide';
+          if (personIndex === index){
+            position = 'activeSlide';
+          }
+          if(personIndex === index-1 || (index === 0 && personIndex === people.length-1)){
+            position='lastSlide';
+          }
+          
+          return(
+            <article key={id} className={position} >
+              <img src={image} alt={name} className='person-img'/>
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className='icon'/>
+            </article>
+          );
+        })}
+        <button type="button" className='prev' onClick={()=>setIndex(index-1)}>
+          <FiChevronLeft/>
+        </button>
+        <button type="button" className='next' onClick={()=>setIndex(index+1)}>
+          <FiChevronRight/>
+        </button>
+      </div>
+    </section>
   )
 }
 
