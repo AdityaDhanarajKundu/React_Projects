8f0ccad9da9f9283c16f40b41ab5f9fc89096bae
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-14 18:06:52 +0530

    Random paragraph generator initial commit

---
+++ paragraph/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import data from './data';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Paragraph</h1>
+    </>
+  )
+}
+
+export default App;
