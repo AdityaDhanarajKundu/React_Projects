50e8d46ff6b40f6ce5b4f3a0b9e2006ca46e957a
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-15 14:17:22 +0530

    added the # to the hex values copy to clipboard function to be added

--- color-code/src/SingleColor.jsx
+++ color-code/src/SingleColor.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-function SingleColor({ rgb, weight, index, hexColor}) {
+function SingleColor({ rgb, weight, index, hexColor }) {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(","); // to convert the rgb number array into a comma separated string to be passed to the rgb() function in the inline css
-//   const hex = rgbToHex(...rgb); // to convert the rgb number array into a hex value
-
+  //   const hex = rgbToHex(...rgb); // to convert the rgb number array into a hex value
+    const hexValue = `#${hexColor}`;
+    
   return (
-    <article className={`color ${index>10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
     </article>
   );
 }
