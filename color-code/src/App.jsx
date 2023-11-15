81892f93b1ea08413f0cb03432510409390f4588
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-15 12:28:11 +0530

    getting the values of the colors from the hex input codes and the handling the error on wrong input

--- color-code/src/App.jsx
+++ color-code/src/App.jsx
@@ -6,20 +6,38 @@ function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false); // boolean state to check if correct color value is provided
   // state to store all the shades of a color
-  const [list,setList] = useState([]);
-  
-  function handleSubmit(e){
+  const [list, setList] = useState([]);
+
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   }
-  
+
   return (
     <>
       <section className="container">
         <h3>Color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" name="color" id="color" placeholder="#f15025" value={color} onChange={(e)=>{setColor(e.target.value)}}/>
-          <button type="submit" className="btn">Submit</button>
+          <input
+            type="text"
+            name="color"
+            id="color"
+            placeholder="#f15025"
+            value={color}
+            className={`${error ? "error" : null}`}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
         </form>
       </section>
       <section className="colors">
