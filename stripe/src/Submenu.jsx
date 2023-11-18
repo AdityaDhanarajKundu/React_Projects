8b6e600d3971d173a78830c88a3d7d8fff1cc7c0
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-18 15:52:42 +0530

    dynamically changing the submenu width based on the content of the sublinks present in the submenu

--- stripe/src/Submenu.jsx
+++ stripe/src/Submenu.jsx
@@ -4,13 +4,21 @@ import { useGlobalContext } from "./context";
 function Submenu() {
   const { isSubmenuOpen, location, page:{page,links} } = useGlobalContext();
   const container = useRef(null);
+  const [columns, setColumns] = useState("col-2");
 
   useEffect(() => {
+    setColumns("col-2");
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+    if(links.length === 3){
+        setColumns("col-3");
+    }
+    if(links.length > 3){
+        setColumns("col-4");
+    }
+  }, [location,links]);
 
   return (
     <aside
@@ -18,7 +26,7 @@ function Submenu() {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
             const {label,icon,url} = link;
             return(
