103ee8c7c6c48be5697446ad212a6dc949b80ff0
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-22 23:26:29 +0530

    completed the search input field

--- cocktails/src/components/SearchForm.jsx
+++ cocktails/src/components/SearchForm.jsx
@@ -1,12 +1,24 @@
 import { useGlobalContext } from "../context";
+import { useRef, useEffect } from "react";
 
 function SearchForm(){
     const { setSearchTerm } = useGlobalContext();
+    const searchValue = useRef("");
+
+    useEffect(() => {
+        searchValue.current.focus();
+    }, []);
     
     return(
-        <div>
-            <h2>search form</h2>
-        </div>
+        <section className="section search">
+            <form className="search-form">
+                <div className="form-control">
+                    {/* uncontrolled and dynamic handling of input values that is using useRef rather than state values and avoiding use the value attribute */}
+                    <label htmlFor="name">search your favorite cocktail</label>
+                    <input type="text" id="name" ref={searchValue} onChange={() => setSearchTerm(searchValue.current.value)} />
+                </div>
+            </form>
+        </section>
     );
 }
 
