8c3aa114f3aad6bbd3abe0fcb8db35c44713a5a7
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-22 21:40:16 +0530

    fetching the drinks information and destructuring the respective values from the response

--- cocktails/src/context.jsx
+++ cocktails/src/context.jsx
@@ -10,6 +10,45 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
+
+  // function to fetch data from the api
+  async function fetchDrinks(){
+    setLoading(true); // every time we fetch data, it should show the loading animation
+    try{
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
+      // destructuring the data to get the drinks array
+      const {drinks} = data;
+      if(drinks){
+        const newCocktails = drinks.map((item) => {
+          // destructuring the data to get the id, name, image and info
+          const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          }
+        });
+        setCocktails(newCocktails);
+      }
+      else{
+        setCocktails([]);
+      }
+      setLoading(false);
+    }
+    catch(error){
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  // useEffect hook to fetch data from the api
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm]);
   
   return (
     <AppContext.Provider
