8b64016ecf2a4aa210ce34bff5e0611dea2923d8
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-11-23 01:29:16 +0530

    fetching individual cocktail details from the api and passing it to be added into the cocktail state value/variable

--- cocktails/src/pages/SingleCocktail.jsx
+++ cocktails/src/pages/SingleCocktail.jsx
@@ -1,12 +1,77 @@
-import {useParams, Link} from 'react-router-dom';
+import {useParams, NavLink} from 'react-router-dom';
 import Loading from '../components/Loading';
+import {useState, useEffect} from 'react';
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
 function SingleCocktail(){
+    const {id} = useParams(); //useParams is a hook that allows you to access the parameters from the URL in functional components
+    const [loading, setLoading] = useState(false);
+    const [cocktail, setCocktail] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        async function getCocktail(){
+            try{
+                const response = await fetch(`${url}${id}`);
+                const data = await response.json();
+                
+                if(data.drinks){
+                    // destructuring the data to get the id, name, image and info
+                    const {
+                        strDrink: name,
+                        strDrinkThumb: image,
+                        strAlcoholic: info,
+                        strCategory: category,
+                        strGlass: glass,
+                        strInstructions: instructions,
+                        strIngredient1,
+                        strIngredient2,
+                        strIngredient3,
+                        strIngredient4,
+                        strIngredient5,
+                    } = data.drinks[0];
+                    const ingredients = [
+                        strIngredient1,
+                        strIngredient2,
+                        strIngredient3,
+                        strIngredient4,
+                        strIngredient5,
+                    ];
+                    const newCocktail = {
+                        name,
+                        image,
+                        info,
+                        category,
+                        glass,
+                        instructions,
+                        ingredients,
+                    };
+                    setCocktail(newCocktail);
+                }
+                else{
+                    setCocktail(null);
+                }
+                setLoading(false);
+            }
+            catch(error){
+                console.log(error);
+                setLoading(false);
+            }
+        }
+        getCocktail();
+    }, [id]);
+
+    if(loading){
+        return <Loading />
+    }
+    if(!cocktail){
+        return <h2 className="section-title">no cocktail to display</h2>
+    }
+    
     return(
         <div>
-            <h2>single cocktail page</h2>
+            <h2>{id}</h2>
         </div>
     );
 }
